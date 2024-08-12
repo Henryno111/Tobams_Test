28426e420334132ff80fe2944a31a71bbd33a6c7
Henryno111 <h.agukwe@yahoo.com>
2024-08-12 11:06:07 +0100

    more changes to category component

--- foodieland/app/ui/Categories.tsx
+++ foodieland/app/ui/Categories.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Categories() {
   return (
-    <section className="flex flex-col items-center justify-between px-[16rem] gap-8">
+    <section className="flex flex-col items-center justify-between px-[9rem] gap-8">
       <div className="flex justify-between w-full">
         <p className="pt-5 font-bold text-xl">CATEGORIES</p>
         <button type="button" className="bg-sky-300 border p-5 rounded-t-md rounded-b-md text-[0.75rem] px-3 py-1">View all Categories</button>
